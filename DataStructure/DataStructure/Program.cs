3acdd19118c4a76686f4d1917f80cb3a0d099501
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-04 14:03:57 +0530

    Add UC3 Solution

--- DataStructure/DataStructure/Program.cs
+++ DataStructure/DataStructure/Program.cs
@@ -6,12 +6,19 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-            Stack st = new Stack(3);
-            st.Push(56);
-            st.Push(30);
-            st.Push(70);
-            Console.WriteLine(st.ToString());
-            Console.WriteLine("The Peek Value is " + st.Peek());
+            //Stack st = new Stack(3);
+            //st.Push(56);
+            //st.Push(30);
+            //st.Push(70);
+            //Console.WriteLine(st.ToString());
+            // Console.WriteLine("The Peek Value is " + st.Peek());
+
+            ///UC3-Queue
+            Queue queue = new Queue(3);
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            Console.WriteLine(queue.ToString());
         }
     }
 }
---
+++ DataStructure/DataStructure/Queue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class Queue
+    {
+        int capacity;
+        int[] obj;
+        int front = -1, rear = -1;
+        public Queue(int capacity)
+        {
+            this.capacity = capacity;
+            obj = new int[capacity];
+        }
+        public bool Enqueue(int data)
+        {
+            if (rear == capacity - 1)
+            {
+                Console.WriteLine("Que is Full");
+                return false;
+            }
+            if (rear == -1)
+                front++;
+            obj[++rear] = data;
+            return true;
+        }
+        public override string ToString()
+        {
+            String s = "";
+            for (int i = front; i <= rear && front != -1; i++)
+            {
+                s = s + obj[i] + " ";
+
+            }
+            return s;
+        }
+
+    }
+}
