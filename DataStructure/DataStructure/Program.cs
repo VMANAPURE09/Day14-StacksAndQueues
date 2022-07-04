34f962bed1ac27587ae96e1c2a3720dff2725366
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-04 13:43:44 +0530

    Add UC1 Solution

---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack st = new Stack(3);
+            st.Push(56);
+            st.Push(30);
+            st.Push(70);
+            Console.WriteLine(st.ToString());
+        }
+    }
+}
---
+++ DataStructure/DataStructure/Stack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class Stack
+    {
+		int capacity;
+		int[] obj;
+		int top = -1;
+		public Stack(int capacity)
+		{
+			this.capacity = capacity;
+			obj = new int[capacity];
+		}
+		public bool Push(int data)
+		{
+			if (top == capacity - 1)
+			{
+				Console.WriteLine("Stack overFlow.");
+				return false;
+			}
+			obj[++top] = data;
+			return true;
+		}
+		public override string ToString()
+		{
+			String s = "";
+			for (int i = 0; i < capacity; i++)
+			{
+				s = s + obj[i];
+			}
+			return s;
+		}
+	}
+}
